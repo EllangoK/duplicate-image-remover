105cc8ed2fce9996f42ce653e20c879b25736960
EllangoK <ellangok@bxscience.edu>
2020-07-10 09:46:08 -0400

    Removes Duplicates

--- remove_ssim_mse.py
+++ remove_ssim_mse.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+from skimage.metrics import structural_similarity as ssim
 from tkinter.filedialog import askdirectory
+from collections import defaultdict
 from tkinter import Tk
-import sys
+import numpy as np
 import cv2
+import sys
 import os
 
 
@@ -18,19 +21,24 @@ def printProgressBar(iteration, total, prefix='Progress:', suffix='Complete', de
     if iteration == total:
         print()
 
-def dhash(image, hashSize=8):
-    resized_img = cv2.resize(image, (hashSize + 1, hashSize))
-    diff = resized_img[:, 1:] > resized_img[:, :-1]
-    return sum([2 ** i for (i, v) in enumerate(diff.flatten()) if v])
+def merge_common(lists):
+    neigh = defaultdict(set)
+    visited = set()
+    for each in lists:
+        for item in each:
+            neigh[item].update(each)
 
-def dhash_path(path, hashSize=8):
-    if path.endswith(".webm") or path.endswith(".mp4") or path.endswith(".gif"):
-        return None
-    image = cv2.imread(path)
-    if image is None:
-        return None
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    return dhash(image, hashSize=hashSize)
+    def comp(node, neigh=neigh, visited=visited, vis=visited.add):
+        nodes = set([node])
+        next_node = nodes.pop
+        while nodes:
+            node = next_node()
+            vis(node)
+            nodes |= neigh[node] - visited
+            yield node
+    for node in neigh:
+        if node not in visited:
+            yield sorted(comp(node))
 
 def keep_widest_img(data):
     widest_file = max(data, key=lambda i: i[0])
@@ -41,7 +49,8 @@ def keep_widest_img(data):
             os.remove(item[1])
 
 def keep_highest_name(data):
-    num_filenames = [(item[1].split('/')[-1].split(',')[0], item[1]) for item in data]
+    num_filenames = [(item[1].split('/')[-1].split(',')[0], item[1])
+                     for item in data]
     highest_filename = max(num_filenames, key=lambda i: int(i[0]))
     widest_file = max(data, key=lambda i: int(i[0]))
     img = cv2.imread(widest_file[1])
@@ -49,7 +58,13 @@ def keep_highest_name(data):
         os.remove(item[1])
     a = cv2.imwrite(highest_filename[1], img)
 
+def mse(first_img, second_img):
+    err = np.sum((first_img.astype("float") - second_img.astype("float")) ** 2)
+    err /= float(first_img.shape[0] * first_img.shape[1])
+    return err
+
 if __name__ == '__main__':
+    custom = True
     args = sys.argv[1:]
     directory = ""
     if len(args) == 0:
@@ -57,31 +72,38 @@ if __name__ == '__main__':
         directory = askdirectory()
     else:
         directory = os.path.abspath(args[0])
-    custom = True
+	
+    image_data = []
 
-    pic_hashes = {}
-
-    print("Calculating dhashes")
+    print("Loading Image, Grayscaling and Rescaling")
     printProgressBar(0, len(os.listdir(directory)))
     for i, rel_path in enumerate(os.listdir(directory)):
         path = directory + "/" + rel_path
-        image_hash = dhash_path(path)
-        printProgressBar(i + 1, len(os.listdir(directory)))
-        if image_hash is None:
+        img = cv2.imread(path, 0)
+        if img is None:
+            printProgressBar(i + 1, len(os.listdir(directory)))
             continue
-        elif image_hash in pic_hashes:
-            pic_hashes[image_hash].append(path)
-        else:
-            pic_hashes[image_hash] = [path]
-
+        image_data.append(
+            [path, cv2.resize(img, (8, 8), interpolation=cv2.INTER_AREA)])
+        printProgressBar(i + 1, len(os.listdir(directory)))
+	
     dupe_list = []
-    for key in pic_hashes.keys():
-        if len(pic_hashes[key]) > 1:
-            dupe_list.append(pic_hashes[key])
-    
-    if len(dupe_list) == 0:
-        print("No Duplicates Found")
-        sys.exit()
+    not_dupe = []
+
+    print("\nCalculating MSE and SSIM")
+    printProgressBar(0, len(image_data))
+    for i, data in enumerate(image_data):
+        mse_ssim = [(item[0], mse(data[1], item[1]), ssim(data[1], item[1]))
+               for item in image_data if data[0] is not item[0]]
+        dupe = [item[0] for item in mse_ssim if item[2] > 0.85 and item[1] < 1]
+        if dupe != []:
+            dupe.insert(0, data[0])
+            dupe_list.append(dupe)
+        else:
+            not_dupe.append(data[0])
+        printProgressBar(i + 1, len(image_data))
+
+    dupe_list = list(merge_common(dupe_list))
 
     count = 0
     for i in dupe_list:
